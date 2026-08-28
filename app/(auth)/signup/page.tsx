8e8b00c57c3eeb0ba7b48813bc568ea96e8845dd
neofrankuy21 <neofrankuy21@gmail.com>
2026-08-28 16:53:37 +0800

    Add Auth Page and Layout

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,24 @@
+import AuthLayout from "../_component/auth_layout";
+import { Input, Label } from "@heroui/react";
+import { Separator } from '@heroui/react';
+import { Button } from "@heroui/react";
+
+export default function SignUp() {
+    return (
+        <AuthLayout>
+            <div className="text-center mb-5 uppercase text-2xl font-bold ">Sign Up</div>
+                  <Separator className="my-4" />
+            <div className="flex w-80 flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                    <Label htmlFor="input-type-email">Email</Label>
+                    <Input id="input-type-email" placeholder="example@example.com" type="email" />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <Label htmlFor="input-type-password">Password</Label>
+                    <Input id="input-type-password" placeholder="Password" type="password" />
+                </div>
+            </div>
+            <Button className="mt-4 w-80">Sign Up</Button>
+        </AuthLayout>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,9 +13,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-muted">{children}</body>
     </html>
   );
 }
